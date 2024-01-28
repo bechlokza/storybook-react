63e4f03ce7fe41ea970b64e3be9ea8d59ef35773
bechlokza <jesper@bechlokza.com>
2024-01-28 12:16:05 +0100

    #clear again

--- storybook-react/src/App.jsx
+++ storybook-react/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 					/>
 				</a>
 			</div>
-			<h1 className='text-red-600 header'>Vite + React</h1>
+			<h1 className='text-green-700 text-7xl header'>Vite + React</h1>
 			<div className='card'>
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
